da1c34122c8a14585347819a3f421b1caea7bac9
thuy-tien1525 <thuytien010524@gmail.com>
2026-05-08 21:38:08 +0700

    fix github actions headless

--- conftest.py
+++ conftest.py
@@ -10,16 +10,19 @@ def driver():
 
     chrome_options = Options()
 
+    # BẮT BUỘC CHO GITHUB ACTIONS
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--headless")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
         options=chrome_options
     )
 
+    # WAIT
     driver.implicitly_wait(10)
 
     yield driver
